2639affe07a705066fa98687dffb8e04e3227dd5
Travis Heaver <travisheaver@Traviss-MacBook-Pro.local>
2013-11-24 11:53:35 -0600

    added test class

---
+++ Pirate Adventure/PATestClass.h
@@ -0,0 +1,13 @@
+//
+//  PATestClass.h
+//  Pirate Adventure
+//
+//  Created by Travis Heaver on 11/24/13.
+//  Copyright (c) 2013 Travis Heaver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PATestClass : NSObject
+
+@end
---
+++ Pirate Adventure/PATestClass.m
@@ -0,0 +1,13 @@
+//
+//  PATestClass.m
+//  Pirate Adventure
+//
+//  Created by Travis Heaver on 11/24/13.
+//  Copyright (c) 2013 Travis Heaver. All rights reserved.
+//
+
+#import "PATestClass.h"
+
+@implementation PATestClass
+
+@end
